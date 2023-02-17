5f5da2d1deb62a924ed3f11be445110603501cb0
Gunblade <mohamed.dev.egy@gmail.com>
2023-02-17 16:55:16 +0200

    white overlay fix attemp 1

--- js/main.js
+++ js/main.js
@@ -43,3 +43,14 @@ if (homeButton) {
     }, 600);
   });
 }
+
+window.addEventListener("pageshow", function (event) {
+  var historyTraversal =
+    event.persisted ||
+    (typeof window.performance != "undefined" &&
+      window.performance.navigation.type === 2);
+  if (historyTraversal) {
+    // Handle page restore.
+    window.location.reload();
+  }
+});
