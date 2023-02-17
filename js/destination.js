dce1666098d554c4153aaab532cf41cf0f80f826
Gunblade <mohamed.dev.egy@gmail.com>
2023-02-17 15:41:20 +0200

    fixrd destination content change

--- js/destination.js
+++ js/destination.js
@@ -1,15 +1,61 @@
-// import { mainButtonClicked } from "./main.js";
+// DESTINATION
 
-// const destinationMain = document.getElementById(`destination`);
-// console.log(mainButtonClicked);
+const destinationLinks = document.querySelectorAll(
+  `#destination section ul li a`
+);
+const planetName = document.getElementById(`planetName`);
+const planetDescription = document.getElementById(`planetDescription`);
+const distance = document.getElementById(`distance`);
+const travelTime = document.getElementById(`travelTime`);
+const planetImg = document.getElementById(`planetImg`);
+const changingElements = document.querySelectorAll(`.changing`);
 
-// if (mainButtonClicked === `true`) {
-//   destinationMain.classList.add(`remove-overlay`);
-//   setTimeout(() => {
-//     destinationMain.classList.remove(`make-overlay`);
-//     destinationMain.classList.remove(`remove-overlay`);
-//   }, 1000);
-// } else {
-//   destinationMain.classList.remove(`make-overlay`);
-//   destinationMain.classList.remove(`remove-overlay`);
-// }
+let DestTarget;
+
+if (destinationLinks) {
+  destinationLinks.forEach((link) => {
+    // change active class on click
+    link.addEventListener(`click`, () => {
+      destinationLinks.forEach((link) => link.classList.remove(`active`));
+      link.classList.add(`active`);
+
+      // hide all changing elements
+      changingElements.forEach((el) => (el.style.opacity = `0`));
+
+      // change page content
+      // get the index of the clicked link
+
+      setTimeout(() => {
+        DestTarget = link.dataset.index;
+
+        fetch("/data.json")
+          .then((res) => res.json())
+          .then((data) => data.destinations)
+          .then((data) => {
+            planetName.innerHTML = data[DestTarget].name;
+            return data;
+          })
+          .then((data) => {
+            planetImg.src = data[DestTarget].images.webp;
+            return data;
+          })
+          .then((data) => {
+            planetDescription.innerHTML = data[DestTarget].description;
+            return data;
+          })
+          .then((data) => {
+            distance.innerHTML = data[DestTarget].distance;
+            return data;
+          })
+          .then((data) => {
+            travelTime.innerHTML = data[DestTarget].travel;
+            return data;
+          });
+
+        setTimeout(() => {
+          changingElements.forEach((el) => (el.style.opacity = `1`));
+        }, 460);
+      }, 280);
+    });
+  });
+}
