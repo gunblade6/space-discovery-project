a3d11093bf92f275c35da04e85b593315e014858
Gunblade <mohamed.dev.egy@gmail.com>
2023-02-17 07:23:15 +0200

    crew page (responsive)

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ const travelTime = document.getElementById(`travelTime`);
 const planetImg = document.getElementById(`planetImg`);
 const changingElements = document.querySelectorAll(`.changing`);
 
-let target;
+let DestTarget;
 
 if (destinationLinks) {
   destinationLinks.forEach((link) => {
@@ -72,28 +72,28 @@ if (destinationLinks) {
       // get the index of the clicked link
 
       setTimeout(() => {
-        target = link.dataset.index;
+        DestTarget = link.dataset.index;
 
         fetch("/data.json")
           .then((res) => res.json())
           .then((data) => data.destinations)
           .then((data) => {
-            planetName.innerHTML = data[target].name;
+            planetName.innerHTML = data[DestTarget].name;
             return data;
           })
           .then((data) => {
-            planetImg.src = data[target].images.webp;
+            planetImg.src = data[DestTarget].images.webp;
           })
           .then((data) => {
-            planetDescription.innerHTML = data[target].description;
+            planetDescription.innerHTML = data[DestTarget].description;
             return data;
           })
           .then((data) => {
-            distance.innerHTML = data[target].distance;
+            distance.innerHTML = data[DestTarget].distance;
             return data;
           })
           .then((data) => {
-            travelTime.innerHTML = data[target].travel;
+            travelTime.innerHTML = data[DestTarget].travel;
             return data;
           });
 
@@ -105,9 +105,54 @@ if (destinationLinks) {
   });
 }
 
-window.addEventListener(`touchstart`, () => {
-  if (homeButton) {
-    homeButton.classList.remove(`clicked`);
-    homeButton.blur();
-  }
-});
+const crewLinks = document.querySelectorAll(`#crew section ul li a`);
+const crewName = document.getElementById(`crewName`);
+const crewDescription = document.getElementById(`crewDescription`);
+const crewImg = document.getElementById(`crewImg`);
+const crewRole = document.getElementById(`crewRole`);
+
+let crewTarget;
+
+if (crewLinks) {
+  crewLinks.forEach((link) => {
+    // change active class on click
+    link.addEventListener(`click`, () => {
+      crewLinks.forEach((link) => link.classList.remove(`active`));
+      link.classList.add(`active`);
+
+      // hide all changing elements
+      changingElements.forEach((el) => (el.style.opacity = `0`));
+
+      // change page content
+      // get the index of the clicked link
+
+      setTimeout(() => {
+        crewTarget = link.dataset.index;
+
+        fetch("/data.json")
+          .then((res) => res.json())
+          .then((data) => data.crew)
+          .then((data) => {
+            crewName.innerHTML = data[crewTarget].name;
+            return data;
+          })
+          .then((data) => {
+            crewImg.src = data[crewTarget].images.webp;
+            return data;
+          })
+          .then((data) => {
+            crewRole.innerHTML = data[crewTarget].role;
+            return data;
+          })
+          .then((data) => {
+            crewDescription.innerHTML = data[crewTarget].bio;
+            return data;
+          });
+
+        setTimeout(() => {
+          changingElements.forEach((el) => (el.style.opacity = `1`));
+        }, 350);
+      }, 350);
+    });
+  });
+}
